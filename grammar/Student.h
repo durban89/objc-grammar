aed8f0149f1c3c326cc8e9d7769bda39a92e254c
dapeng126 <zhangdapeng89@126.com>
2016-12-09 08:21:04 +0800

    add Student

---
+++ grammar/Student.h
@@ -0,0 +1,16 @@
+//
+//  Student.h
+//  grammar
+//
+//  Created by durban on 16/12/9.
+//  Copyright © 2016年 durban126. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@property (assign, nonatomic) int age;
+@property (assign, atomic) NSString *name;
+
+@end
---
+++ grammar/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  grammar
+//
+//  Created by durban on 16/12/9.
+//  Copyright © 2016年 durban126. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
